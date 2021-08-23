6f98fdf07c9a0c2da3190f747d53710ed751e8a0
Bekbolisemo <beka228beka882@gmail.com>
2021-08-23 23:27:34 +0600

    Initial commit

---
+++ src/kg/geektech/game/players/Berserk.java
@@ -0,0 +1,33 @@
+package kg.geektech.game.players;
+
+import kg.geektech.game.general.RPG_Game;
+
+
+public class Berserk extends Hero {
+    public Berserk(int health, int damage) {
+        super(health, damage,
+                SuperAbility.SAVE_DAMAGE_AND_REVERT);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+
+        switch (RPG_Game.random.nextInt(3)+1) {
+
+            case 1 :
+                heroes[3].setDamage( heroes[3].getDamage() +  RPG_Game.random.nextInt(15) + 1 );
+                System.out.println("Берсерк октевировал супер спасобность ");
+                break;
+
+            case 2 :
+                heroes[3].setDamage(10);
+                break;
+            case 3 :
+                heroes[3].setDamage(10);
+                break;
+
+        }
+
+
+        }
+   }
---
+++ src/kg/geektech/game/players/Magic.java
@@ -0,0 +1,32 @@
+package kg.geektech.game.players;
+
+import kg.geektech.game.general.RPG_Game;
+
+import java.util.Random;
+
+public class Magic extends Hero {
+    public Magic(int health, int damage) {
+        super(health, damage, SuperAbility.BOOST);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        for (int i = 0; i < heroes.length; i++) {
+            switch (RPG_Game.random.nextInt(3) + 1) {
+                case 1:
+                    heroes[i].setDamage(getDamage() + RPG_Game.random.nextInt(5) + 2);
+                    System.out.println("Медик использовал супер спасобность");
+                    break;
+
+                case 2:
+                    heroes[i].setDamage(getDamage() + 0);
+                    break;
+                case 3:
+                    heroes[i].setDamage(getDamage() + 0);
+                    break;
+            }
+            break;
+
+        }
+    }
+}
